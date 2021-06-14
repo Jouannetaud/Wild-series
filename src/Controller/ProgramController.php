8bb20f1a5594b8c80036d0999f9112554211eb50
jouannetaud <remijouannetaud10@gmail.com>
2021-06-14 11:03:23 +0200

    quest 10 relation bidirectionnel

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -1,47 +1,116 @@
 <?php
-// src/Controller/ProgramController.php
+
 namespace App\Controller;
 
 use App\Entity\Program;
+use App\Form\ProgramType;
 use App\Repository\ProgramRepository;
+use App\Repository\SeasonRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/program")
+ */
 class ProgramController extends AbstractController
 {
     /**
-     * @Route("/programs/", name="program_index")
-     * @return Response A response instance
+     * @Route("/", name="program_index", methods={"GET"})
      */
-
     public function index(ProgramRepository $programRepository): Response
     {
-        $programs = $programRepository->findAll();
-    
-        return $this->render('program/index.html.twig',['programs'=> $programs]);
-            
-    }
-
-    
-    /**
-     * @Route("/programs/{id}" , methods={"GET"}, requirements={"page"="\d+"}, name= "program_show")
-     */
-
-    public function show(int $id):Response
-    {
-        $program = $this->getDoctrine()
-            ->getRepository(Program::class)
-            ->findOneBy(['id' => $id]);
-    
-        if (!$program) {
-            throw $this->createNotFoundException(
-                'No program with id : '.$id.' found in program\'s table.'
-            );
-        }
-        return $this->render('program/show.html.twig', [
-            'program' => $program,
+        return $this->render('program/index.html.twig', [
+            'programs' => $programRepository->findAll(),
         ]);
     }
 
+    /**
+     * @Route("/new", name="program_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $program = new Program();
+        $form = $this->createForm(ProgramType::class, $program);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($program);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('program_index');
+        }
+
+        return $this->render('program/new.html.twig', [
+            'program' => $program,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="program_show", methods={"GET"})
+     */
+    public function show(Program $program ): Response
+    {
+
+      
+
+        return $this->render('program/show.html.twig', [
+            'program' => $program,
+            
+
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="program_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Program $program): Response
+    {
+        $form = $this->createForm(ProgramType::class, $program);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('program_index');
+        }
+
+        return $this->render('program/edit.html.twig', [
+            'program' => $program,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="program_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Program $program): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$program->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($program);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('program_index');
+    }
+
+    /**
+     * @Route("/{programId}/seasons/{seasonId}", name="program_season_show")
+     */
+    public function showSeason(int $programId, int $seasonId , SeasonRepository $seasonRepository): Response
+    {
+        $season = $seasonRepository->find($seasonId);
+
+        return $this->render('program/season_show.html.twig', [
+           
+            'season' => $season,
+            
+        ]);
+    }
+
+    
 }
--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -15,6 +15,7 @@ class CategoryFixtures extends Fixture
         'Animation',
         'Fantastique',
         'Horreur',
+        'Comedie',
     ];
 
 
